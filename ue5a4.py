ee8a763f0fccfeb120213f439c78b89ea6b0af5a
linussuni <ziegert.l@googlemail.com>
2017-06-09 11:47:04 +0200

    Kommentierung

--- ue5a4.py
+++ ue5a4.py
@@ -5,18 +5,17 @@ import csv
 import re
 from collections import Counter
 
-
-def countsSortedAlphabetically(counter, **kw):
-	return sorted(counter.items(), **kw)
-
+#****************************************************************
+#von Stackoverflow außschließlich optische Funktion
 def countsSortedNumerically(counter, **kw):
 	return sorted(counter.items(), key=lambda x:x[1], **kw)
 
-# `from pprint import pprint as pp` is also useful
 def printByLine(tuples):
 	print( '\n'.join(' '.join(map(str,t)) for t in tuples) )
+#****************************************************************
 
-def getPage(url):
+
+def getPage(url):#genauso wie im Beispiel
 	r= requests.get(url)
 	data = r.text
 	spobj = BeautifulSoup(data, "lxml")
@@ -25,37 +24,42 @@ def getPage(url):
 def main():
 
 	fobj = open('heise-themen-https.csv', 'w')
-	csvw = csv.writer(fobj)
+	csvw = csv.writer(fobj) #es wird eine csv geschrieben
+			#ja sie wird nicht benutzt, diente aber dem zweck eine anfängliche
+		#Übersicht zu haben ob das richtige raus kommt und für das Erfolgsgefühl
 
-	completetextstr = ""
+	completetextstr = ""#string aller Wörter um diese später in liste zu bringen
 
-	for page in range(0,4,1):
+	for page in range(0,4,1): #iteriere durch die 4 Seiten
 
 		heise_themen_url = "https://www.heise.de/thema/https?seite="+str(page)
 		content = getPage(heise_themen_url).findAll('div', class_="keywordliste")
+				#die Überschriften stehen immer im div mit der class keyw...
 		for c in content:
 			c = c.findAll("header")
 			txt = [""]
 			#itercontent = iter(content)
 			#next(itercontent) #to exclude first header"alle Beitraege zu https"
+			#funktionierte leider nicht wie gewünscht
 			for t in c:
 				txt.append(t.text.encode('utf-8'))
 
 			csvw.writerow(txt)
 			
 			completetextstr += " ".join(txt)
-			
-	words = re.findall(ur'\w+', completetextstr, re.UNICODE)
-	cap_words = [word.upper() for word in words]
 	
+	#alle Wörter werden gelistet			
+	words = re.findall(ur'\w+', completetextstr, re.UNICODE) 
+	#großmachen der wörter für weniger unterschiede
+	cap_words = [word.upper() for word in words]
+	#counter für die Wörter	
 	word_counts = Counter(cap_words)
 	
 	fobj.close()
 	print("\nDONE ! heise.de/thema/https was scraped completely.\n")
-	print("content:  "+completetextstr)
+	print("content:  "+completetextstr) #schöne Übersicht über alle Überschriften
 	printByLine(countsSortedNumerically(word_counts, reverse=True))
-	#print(words)
-	#print(word_counts)
+		#schöne Darstellung der verwendeten Wörter
 
 if __name__ == '__main__':
 	main()
